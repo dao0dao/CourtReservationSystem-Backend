e9fd1f28e168630e13371d287ccb73484b7aa7a5
Demid <d_greshnikov@tlen.pl>
2021-12-19 13:37:52 +0100

    zmiana skturktury głównego folderu

--- utils/database.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Sequelize = require('sequelize').Sequelize;
-var sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
-    host: process.env.MYSQL_HOST,
-    dialect: process.env.MYSQL_DIALECT
-});
-exports["default"] = sequelize;
