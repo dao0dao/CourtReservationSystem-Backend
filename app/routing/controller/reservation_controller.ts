82073b8b20528bafa33b829ca73d9238a23875e0
Demid <d_greshnikov@tlen.pl>
2022-07-16 16:37:40 +0200

    dodanie atrybutów do rezerwacji

--- app/routing/controller/reservation_controller.ts
+++ app/routing/controller/reservation_controller.ts
@@ -105,7 +105,7 @@ export default class Timetable {
             where: {
                 date
             },
-            attributes: ['id', 'transformX', 'transformY', 'ceilHeight', 'zIndex', 'date', 'timeFrom', 'timeTo', 'court', 'playerOneId', 'playerTwoId', 'guestOne', 'guestTwo', 'hourCount']
+            attributes: ['id', 'transformX', 'transformY', 'ceilHeight', 'zIndex', 'date', 'timeFrom', 'timeTo', 'court', 'playerOneId', 'playerTwoId', 'guestOne', 'guestTwo', 'hourCount', 'isPlayerOnePayed', 'isPlayerTwoPayed']
         }).catch(err => { if (err) { return databaseFailed(this.res); } });
         reservationArr.forEach(r => {
             const { id, transformX, transformY, ceilHeight, zIndex, date, timeFrom, timeTo, court, playerOneId, playerTwoId, guestOne, guestTwo, hourCount, isPlayerOnePayed, isPlayerTwoPayed } = r;
